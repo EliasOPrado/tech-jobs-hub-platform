927cf5ee013a8720c030cf7a78b628e3f7c91280
EliasOPrado <eliaspradoprofessional@outlook.com>
2024-02-29 16:34:10 -0300

    Initial commit

---
+++ core/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Base(models.Model):
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Technology(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Company(Base):
+    company_name = models.CharField(max_length=255)
+    manager = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="companies_managed"
+    )
+    email = models.EmailField()
+
+    def __str__(self):
+        return str(self.company_name)
+
+
+class Applicant(Base):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=255)
+    description = models.TextField()
+    technologies = models.TextField()
+
+    def __str__(self):
+        return f"Applicant: {self.user.username}"
+
+
+class JobPost(Base):
+    job_title = models.CharField(max_length=255)
+    job_description = models.TextField()
+    company = models.ForeignKey(
+        Company, on_delete=models.CASCADE, related_name="job_posts"
+    )
+    applicants = models.ManyToManyField(Applicant, related_name="job_application")
+
+    def __str__(self):
+        return str(self.job_title)
